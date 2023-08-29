15e5ea6cd61e10a9e53e58c65dc4b0960eeee86a
Joabe <joabeslopes.jsl@gmail.com>
2023-08-29 18:00:07 -0300

    add buttons and change size

--- src/calculator/guiCalculator.java
+++ src/calculator/guiCalculator.java
@@ -8,11 +8,16 @@ import java.awt.FlowLayout;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.SwingConstants;
 
 public class guiCalculator extends JFrame {
 
 	final int WIDHT = 330;
-	final int HEIGHT = 400;
+	final int HEIGHT = 380;
+	private JTextField textField;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -40,7 +45,38 @@ public class guiCalculator extends JFrame {
 		panel.setBackground(new Color(129, 61, 156));
 		getContentPane().add(panel);
 		panel.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setHorizontalAlignment(SwingConstants.CENTER);
+		textField.setFont(new Font("Dialog", Font.PLAIN, 20));
+		textField.setBounds(12, 12, 306, 50);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnAdd = new JButton("+");
+		btnAdd.setFont(new Font("Dialog", Font.BOLD, 22));
+		btnAdd.setBounds(25, 74, 125, 72);
+		panel.add(btnAdd);
+		
+		JButton btnSub = new JButton("-");
+		btnSub.setFont(new Font("Dialog", Font.BOLD, 22));
+		btnSub.setBounds(176, 74, 125, 72);
+		panel.add(btnSub);
+		
+		JButton btnMult = new JButton("*");
+		btnMult.setFont(new Font("Dialog", Font.BOLD, 22));
+		btnMult.setBounds(25, 171, 125, 72);
+		panel.add(btnMult);
+		
+		JButton btnDiv = new JButton("/");
+		btnDiv.setFont(new Font("Dialog", Font.BOLD, 22));
+		btnDiv.setBounds(176, 171, 125, 72);
+		panel.add(btnDiv);
+		
+		JButton btnResult = new JButton("=");
+		btnResult.setFont(new Font("Dialog", Font.BOLD, 22));
+		btnResult.setBounds(100, 260, 125, 72);
+		panel.add(btnResult);
 
 	}
-
 }
